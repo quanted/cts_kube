8db2622fc986ede0c7acfdb37284dbd266fcea2a
popenc <i.nickpope@gmail.com>
2022-09-05 13:59:20 -0400

    updated image refs for dev-aws; added apply manifest script for using env vars in manifests

---
+++ k8s/apply-manifests.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+export $(grep -v '^#' .env | xargs)  # sets env vars with .env
+
+# Applys manifests with env vars:
+envsubst < k8s/cts-configmap.yml | kubectl apply -f -
+# envsubst < k8s/cts-biotransformer-deployment.yml | kubectl apply -f -
+# envsubst < k8s/cts-biotransformer-service.yml | kubectl apply -f -
+envsubst < k8s/cts-celery-deployment.yml | kubectl apply -f -
+envsubst < k8s/cts-django-deployment.yml | kubectl apply -f -
+envsubst < k8s/cts-django-service.yml | kubectl apply -f -
+# envsubst < k8s/cts-envipath-deployment.yml | kubectl apply -f -
+# envsubst < k8s/cts-envipath-service.yml | kubectl apply -f -
+envsubst < k8s/cts-mongodb-service.yml | kubectl apply -f -
+envsubst < k8s/cts-mongodb-statefulset.yml | kubectl apply -f -
+envsubst < k8s/cts-nginx-deployment.yml | kubectl apply -f -
+envsubst < k8s/cts-nginx-service.yml | kubectl apply -f -
+envsubst < k8s/cts-nodejs-deployment.yml | kubectl apply -f -
+envsubst < k8s/cts-nodejs-service.yml | kubectl apply -f -
+# envsubst < k8s/cts-operaws-deployment.yml | kubectl apply -f -
+# envsubst < k8s/cts-operaws-service.yml | kubectl apply -f -
+envsubst < k8s/cts-redis-deployment.yml | kubectl apply -f -
+envsubst < k8s/cts-redis-service.yml | kubectl apply -f -
+envsubst < k8s/cts-tomcat-deployment.yml | kubectl apply -f -
+envsubst < k8s/cts-tomcat-service.yml | kubectl apply -f -

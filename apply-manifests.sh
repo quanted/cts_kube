aee65152489370d852f5becb4e6768032ea31497
popenc <i.nickpope@gmail.com>
2022-09-13 13:18:49 -0400

    added biotransformer, operaws, and envipath to cts stack

--- apply-manifests.sh
+++ apply-manifests.sh
@@ -4,21 +4,21 @@ export $(grep -v '^#' .env | xargs)  # sets env vars with .env
 
 # Applys manifests with env vars:
 envsubst < k8s/cts-configmap.yml | kubectl apply -f -
-# envsubst < k8s/cts-biotransformer-deployment.yml | kubectl apply -f -
-# envsubst < k8s/cts-biotransformer-service.yml | kubectl apply -f -
+envsubst < k8s/cts-biotransformer-deployment.yml | kubectl apply -f -
+envsubst < k8s/cts-biotransformer-service.yml | kubectl apply -f -
 envsubst < k8s/cts-celery-deployment.yml | kubectl apply -f -
 envsubst < k8s/cts-django-deployment.yml | kubectl apply -f -
 envsubst < k8s/cts-django-service.yml | kubectl apply -f -
-# envsubst < k8s/cts-envipath-deployment.yml | kubectl apply -f -
-# envsubst < k8s/cts-envipath-service.yml | kubectl apply -f -
+envsubst < k8s/cts-envipath-deployment.yml | kubectl apply -f -
+envsubst < k8s/cts-envipath-service.yml | kubectl apply -f -
 envsubst < k8s/cts-mongodb-service.yml | kubectl apply -f -
 envsubst < k8s/cts-mongodb-statefulset.yml | kubectl apply -f -
 envsubst < k8s/cts-nginx-deployment.yml | kubectl apply -f -
 envsubst < k8s/cts-nginx-service.yml | kubectl apply -f -
 envsubst < k8s/cts-nodejs-deployment.yml | kubectl apply -f -
 envsubst < k8s/cts-nodejs-service.yml | kubectl apply -f -
-# envsubst < k8s/cts-operaws-deployment.yml | kubectl apply -f -
-# envsubst < k8s/cts-operaws-service.yml | kubectl apply -f -
+envsubst < k8s/cts-operaws-deployment.yml | kubectl apply -f -
+envsubst < k8s/cts-operaws-service.yml | kubectl apply -f -
 envsubst < k8s/cts-redis-deployment.yml | kubectl apply -f -
 envsubst < k8s/cts-redis-service.yml | kubectl apply -f -
 envsubst < k8s/cts-tomcat-deployment.yml | kubectl apply -f -

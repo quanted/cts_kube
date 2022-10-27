c491d56e8bd1805b2fd03f2688699c6c81de8fec
popenc <i.nickpope@gmail.com>
2022-10-27 15:17:17 -0400

    moved execution to cts-mongodb-0 as a mongorestore is performed

--- cts-mongodb-volume-setup.sh
+++ cts-mongodb-volume-setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 echo "Starting CTS MongoDB volume setup"
 
-kubectl exec deploy/oms-shell -- /bin/bash -c "mkdir /tmp/cts"
+kubectl exec cts-mongodb-0 -- /bin/bash -c "mkdir /tmp/cts"
 
 echo "Updating files from s3 bucket"
-kubectl exec deploy/oms-shell -- /bin/bash -c "aws s3 cp s3://qed-stg/cts /tmp/cts --recursive"
-kubectl exec deploy/oms-shell -- /bin/bash -c "ls /tmp/cts"
-kubectl exec deploy/oms-shell -- /bin/bash -c "unzip /tmp/cts/cts_mongodb.zip -d /mnt/cts_mongodb"
-kubectl exec deploy/oms-shell -- /bin/bash -c "rm -rf /cts/hms/"
+kubectl exec cts-mongodb-0 -- /bin/bash -c "aws s3 cp s3://qed-stg/cts /tmp/cts --recursive"
+kubectl exec cts-mongodb-0 -- /bin/bash -c "ls /tmp/cts"
+kubectl exec cts-mongodb-0 -- /bin/bash -c "unzip /tmp/cts/cts_mongodb.zip -d /mnt/cts_mongodb"
+kubectl exec cts-mongodb-0 -- /bin/bash -c "rm -rf /cts/hms/"
+kubectl exec cts-mongodb-0 -- /bin/bash -c "mongorestore -d cts /mnt/cts_mongodb"
+kubectl exec cts-mongodb-0 -- /bin/bash -c "rm -rf /mnt/cts_mongodb"
 
 echo "Completed updates from s3 bucket"

28f12c062a136765177ad7d25cee1d2634935dfb
popenc <i.nickpope@gmail.com>
2022-10-31 15:26:54 -0400

    removed deletion of cts pchem data dump

--- cts-mongodb-volume-setup.sh
+++ cts-mongodb-volume-setup.sh
@@ -8,6 +8,6 @@ kubectl exec deploy/oms-shell -- /bin/bash -c "aws s3 cp s3://qed-stg/cts /tmp/c
 kubectl exec deploy/oms-shell -- /bin/bash -c "unzip /tmp/cts/cts_mongodb.zip -d /mnt/cts-mongodb/cts_data_dump"
 kubectl exec deploy/oms-shell -- /bin/bash -c "rm -rf /tmp/cts/"
 kubectl exec cts-mongodb-0 -- /bin/bash -c "mongorestore -d cts /data/db/cts/cts_data_dump"
-kubectl exec cts-mongodb-0 -- /bin/bash -c "rm -rf /data/db/cts/cts_data_dump"
+# kubectl exec cts-mongodb-0 -- /bin/bash -c "rm -rf /data/db/cts/cts_data_dump"
 
 echo "Completed updates from s3 bucket"
